ef3e67c8e7597254598e5fb8fa835e0f839cf059
Ivan <ivakhnenkoid@gmail.com>
2025-12-21 01:11:03 +0200

    Add mailer

---
+++ src/services/news/mailer.py
@@ -0,0 +1,32 @@
+from src.services.database.user_repository import UserRepository
+from src.services.database.news_repository import NewsRepository
+from src.services.database.sent_news_repository import SentNewsRepository
+
+class NewsMailer:
+    """
+    Сервис рассылки новостей пользователям.
+    Отвечает за отправку новых новостей подписанным пользователям
+    и фиксирует факт отправки в базе данных.
+    """
+    def __init__(self, bot, users: UserRepository, news: NewsRepository, sent: SentNewsRepository):
+        self.bot = bot
+        self.users = users
+        self.news = news
+        self.sent = sent
+
+    async def sent_new_news(self):
+        users = self.users.get_subscribed_users()
+        news_list = self.news.get_latest(10)
+
+        for user_id in users:
+            for item in news_list:
+                if self.sent.was_sent(user_id, item["id"]):
+                    continue
+
+                text = (
+                    f"📰 <b>{item['title']}</b>\n"
+                    f"🔗 {item['source_url']}"
+                )
+
+                await self.bot.send_message(user_id, text, parse_mode="HTML")
+                self.sent.mark_sent(user_id, item["id"])
