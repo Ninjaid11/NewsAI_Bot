5c50cabb971cfd7a67195634ef690d32f955248b
Ivan <ivakhnenkoid@gmail.com>
2026-01-24 21:06:20 +0200

    add news_summarizer.py

---
+++ src/services/news/news_summarizer.py
@@ -0,0 +1,23 @@
+from src.services.llm.prompts import summary_prompt
+from src.services.llm.llm_service import LLMService
+
+class NewsSummarizer:
+    """
+    Обрабатывает новость через LLM
+    """
+    def __init__(self, llm: LLMService):
+        self.llm = llm
+
+    def summarize(self, *, title, content, published_at, lang: str) -> str:
+        system, user_template = summary_prompt(lang)
+
+        user_prompt = user_template.format(
+            title=title,
+            content=content,
+            published_at=published_at
+        )
+
+        return self.llm.generate(
+            system_instruction=system,
+            user_prompt=user_prompt
+        )
